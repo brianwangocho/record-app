aef82de98d588cbfe21c0f7d9003759673e5b573
wangocho <bwangocho@gmail.com>
2022-03-16 22:01:59 +0300

    navigation done

--- app/src/main/java/com/example/milking/Dashfragment.kt
+++ app/src/main/java/com/example/milking/Dashfragment.kt
@@ -34,7 +34,7 @@ class Dashragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_dashragment, container, false)
+        return inflater.inflate(R.layout.fragment_dashfragment, container, false)
     }
 
     companion object {
